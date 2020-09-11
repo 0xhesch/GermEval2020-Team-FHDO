0a735e374c15f1b1dfccf8830f9d19ecb99b4899
0xhesch <37747481+0xhesch@users.noreply.github.com>
2020-09-11 11:43:48 +0200

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os, sys
+import pandas as pd
+from fast_bert.prediction import BertClassificationPredictor
+import pickle
+import json
+
+MODEL_PATH = 'output/model_out/'
+
+predictor = BertClassificationPredictor(
+				model_path=MODEL_PATH,
+				label_path='./', # location for labels.csv file
+				multi_label=False,
+				model_type='roberta',
+				do_lower_case=False)
+
+#multi prediction
+test_data = pd.read_csv('dev.csv')
+
+x = 0
+for item in test_data.text:
+	prediction = predictor.predict(item)
+	with open('predictions.tsv', 'a') as fp:
+		#print(str(test_data.id[x]) + '\t' + prediction[0][0][0] + '\t' + prediction[0][0][1] + '\n')
+		fp.write(str(test_data.id[x]) + '\t' + prediction[0][0][0] + '\t' + prediction[0][0][1] + '\n')
+	x = x + 1
